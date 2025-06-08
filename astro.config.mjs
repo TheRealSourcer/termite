725379d995b8b887b9811b49a3dff4f4a7f607b9
Distro <bertolotti2008@gmail.com>
2025-06-08 17:24:54 -0400

    Added SSR (this time with netlify adapter).

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite"
-
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {    
-      plugins: [tailwindcss()],
-  },
-
+  vite: {    plugins: [tailwindcss()] },
   integrations: [react()],
-  output: "server"
+  output: "server",
+  adapter: netlify()
 });
